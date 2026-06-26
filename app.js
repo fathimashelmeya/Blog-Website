04111f9eb7fd2f79ec5b44d6a141f14b6e5e5f72
fathimashelmeya <fathimashelmeya@gmail.com>
2026-06-26 17:48:38 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  {
+    id: "1a",
+    username: "fathima",
+    content: "Neet exam done",
+  },
+  {
+    id: "2b",
+    username: "Shelmeya",
+    content: "Got a JOb baby",
+  },
+  {
+    id: "3c",
+    username: "Max",
+    content: "I love Coding",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id == p.id);
+  res.render("show.ejs", { post });
+});
+app.listen(port, () => {
+  console.log(`The port is Running ${port}`);
+});
